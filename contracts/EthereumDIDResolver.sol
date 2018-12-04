b90ebafd3041355282adf66c0fd93b7a4e0ba5c7
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-12-04 17:07:32 +0100

    Switch to interface

--- contracts/EthereumDIDResolver.sol
+++ contracts/EthereumDIDResolver.sol
@@ -3,7 +3,6 @@ pragma experimental "v0.5.0";
 pragma experimental "ABIEncoderV2";
 
 import './interface/EthereumDIDRegistryInterface.sol';
-import './EthereumDIDRegistry.sol';
 
 contract EthereumDIDResolver {
     address public ethDIDReg;
@@ -13,11 +12,11 @@ contract EthereumDIDResolver {
     }
 
     function isIdentityOwner(address identity, address actor) public view returns (bool) {
-        return EthereumDIDRegistry(ethDIDReg).isIdentityOwner(identity, actor);
+        return EthereumDIDRegistryInterface(ethDIDReg).isIdentityOwner(identity, actor);
     }
 
     function getIdentity(address actor) public view returns (address) {
-        address id = EthereumDIDRegistry(ethDIDReg).getIdentity(actor);
+        address id = EthereumDIDRegistryInterface(ethDIDReg).getIdentity(actor);
         require(id != address(0), '0x0 Identity is not allowed to perform any action');
         return id;
     }
