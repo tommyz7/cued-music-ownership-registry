8ff9c152972c0b50e587245d3927a080c12d8a26
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-12-14 12:57:43 +0100

    Add tests for registry address update

--- test/ownership_royalties_agreements.behaviour.js
+++ test/ownership_royalties_agreements.behaviour.js
@@ -325,6 +325,9 @@ function shouldBehaveLikeInit(Setup) {
     let addr = await Setup.ownershipContractConstant.methods.ethDIDReg().call()
     assert.equal(addr.toLowerCase(), Setup.ethDIDReg.address,
       "EthereumDIDRegistry address has not been set correctly");
+
+    let EthRegistrySet = getEvent(Setup.initialTX, 'EthRegistrySet');
+    EthRegistrySet.reg.toLowerCase().should.eq(Setup.ethDIDReg.address);
   });
 
   it("should mint 100% ownership and royalties to original owner", async () => {
