e148e421c5546514c27e4ea62164b27e562e2cf0
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-01-11 18:39:47 +0100

    Use MusicLibPublic

--- test/music_registry.js
+++ test/music_registry.js
@@ -2,7 +2,8 @@ import { ZERO_ADDRESS } from './utils.js';
 import { shouldBehaveLikeMusicRegistry } from './music_registry.behaviour.js';
 
 var MusicRegistry = artifacts.require("./MusicRegistry.sol");
-var MusicRegistryPublic = artifacts.require("./MusicRegistryPublic.sol");
+var MusicRegistryPublic = artifacts.require("./mock/MusicRegistryPublic.sol");
+var MusicLibPublic = artifacts.require("./mock/MusicLibPublic.sol");
 var MusicLib = artifacts.require("./library/MusicLib.sol");
 
 var ProxyFactory = artifacts.require("./ProxyFactory.sol");
@@ -50,14 +51,6 @@ contract("MusicRegistry", (accounts) => {
     Setup.recordings[i] = recording
   }
 
-  // for (var i = 0; i < 10; i++) {
-  //   Setup.owner[i] = web3.eth.accounts.create();
-  // }
-
-  // for (var i = 0; i < 10; i++) {
-  //   Setup.contributor[i] = web3.eth.accounts.create();
-  // }
-
   before("deploy contracts", async () => {
     Setup.accounts = accounts;
     Setup.admin = accounts[0];
@@ -70,26 +63,29 @@ contract("MusicRegistry", (accounts) => {
     Setup.proxyFactory = await ProxyFactory.new({from: Setup.admin});
 
     Setup.MusicLib = await MusicLib.new({from: Setup.admin});
+    MusicLibPublic.link("MusicLib", Setup.MusicLib.address);
+    Setup.MusicLibPublic = await MusicLibPublic.new({from: Setup.admin});
+
     MusicRegistry.link("MusicLib", Setup.MusicLib.address);
-    MusicRegistryPublic.link("MusicLib", Setup.MusicLib.address);
     Setup.musicRegistryConstant = await MusicRegistry.new({from: Setup.admin});
 
     await Setup.proxyFactory.contract.methods.init(
       Setup.factoryOwner.address,
       Setup.musicRegistryConstant.address,
       Setup.ownershipImpl.address
-    ).send({from: Setup.admin, gas: 500000});
+    ).send({from: Setup.admin, gas: 900000});
 
     Setup.initialTX = await Setup.musicRegistryConstant.contract.methods.init(
       Setup.ethDIDReg.address,
       Setup.proxyFactory.address
-    ).send({from: Setup.admin, gas: 500000});
+    ).send({from: Setup.admin, gas: 900000});
 
+    MusicRegistryPublic.link("MusicLib", Setup.MusicLib.address);
     Setup.musicRegistryPublicConstant = await MusicRegistryPublic.new({from: Setup.admin});
     Setup.initialTX = await Setup.musicRegistryPublicConstant.contract.methods.init(
       Setup.ethDIDReg.address,
       Setup.proxyFactory.address
-    ).send({from: Setup.admin, gas: 500000});
+    ).send({from: Setup.admin, gas: 900000});
   });
 
   beforeEach("deploy new instance of MusicRegistry", async () => {
@@ -97,7 +93,7 @@ contract("MusicRegistry", (accounts) => {
     Setup.initialTX = await Setup.musicRegistry.contract.methods.init(
       Setup.ethDIDReg.address,
       Setup.proxyFactory.address
-    ).send({from: Setup.admin, gas: 500000});
+    ).send({from: Setup.admin, gas: 900000});
   });
 
   shouldBehaveLikeMusicRegistry(Setup);
