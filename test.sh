b78d13923a287f0fda75b05e49a712416a2f866e
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-01-22 14:54:12 +0100

    add test script and test docker compose config

---
+++ test.sh
@@ -0,0 +1,5 @@
+# replace local truffle v4 with global v5 - not sure why but it's necessary
+rm /app/node_modules/.bin/truffle
+ln -s /usr/local/bin/truffle /app/node_modules/.bin/truffle
+truffle version
+truffle test test/proxy_factory.js --network development
