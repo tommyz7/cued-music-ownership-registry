96132255c01df3f6103c26d300e2e801316ba0d3
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-01-22 14:52:46 +0100

    remove not needed deploy script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#----------------------------------------------------------------------------->
-# This script exists so that we can save all output in the specified log file >
-#----------------------------------------------------------------------------->
-
-# deploy contracts
-truffle migrate --reset --all | tee -a $LOG_FILE
-sleep 1d
