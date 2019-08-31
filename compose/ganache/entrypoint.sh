e620c9ebb96f65c86d8143f8fa1e99cb47c0e800
Leandro Anthonioz <leandro.anthonioz@gmail.com>
2019-08-31 20:00:01 -0300

    Fixed entrypoint

--- compose/ganache/entrypoint.sh
+++ compose/ganache/entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Run database migratiosn
-/app/compose/ganache/migrate.sh
+/bin/sh /app/compose/ganache/migrate.sh
 
 # Start ganache-cli process
 ganache-cli -d --networkId 2833 --db /var/ganache-cli/chaindata --account="0xF0F637DBBBB802D5DCEC6E7629F7AEE958235173222E8A5520DACCE760490CE6,1000000000000000000000000000"
