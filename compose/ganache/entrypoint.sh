d1687be931b5fe82eb357cccc82e5a73facb05db
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2019-08-31 22:05:30 -0700

    new ganache smart contract deployment

--- compose/ganache/entrypoint.sh
+++ compose/ganache/entrypoint.sh
@@ -3,8 +3,22 @@
 # If an error occurs during migrations, cancel script
 set -e
 
-# Run database migratiosn
-/bin/sh /app/compose/ganache/migrate.sh
+function shutdownGanache() {
+    echo "Killing existing Ganache CLI process $GANACHE_PID"
+    kill -9 $GANACHE_PID
+}
+
+function startupGanache() {
+    node /app/ganache-core.docker.cli.js -d cued --networkId 2833 --db /var/ganache-cli/chaindata > /dev/null &
+    export GANACHE_PID=$!
+    echo "Started new Ganache CLI as process $GANACHE_PID"
+}
+
+startupGanache
+echo "Preparing smart contracts..."
+truffle migrate --reset --network development
+shutdownGanache
+echo "Smart Contract deployed"
 
 # Start ganache-cli process
-ganache-cli -d --networkId 2833 --db /var/ganache-cli/chaindata --account="0xF0F637DBBBB802D5DCEC6E7629F7AEE958235173222E8A5520DACCE760490CE6,1000000000000000000000000000"
+node /app/ganache-core.docker.cli.js -d cued --blockTime 12 --networkId 2833 --db /var/ganache-cli/chaindata
